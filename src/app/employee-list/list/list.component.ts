a4e29df5ba9ede9299e50708ecbee03f34baa7e3
mpierce <mpierce>
2018-06-15 08:16:28 -0600

    add Employee Data dependency to dummy components

--- src/app/employee-list/list/list.component.ts
+++ src/app/employee-list/list/list.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { EmployeeData } from '../../shared/list-generator.service';
 import { List } from 'immutable';
 
 @Component({
